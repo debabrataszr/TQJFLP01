bf9ac27afc4781cb195e021b4fdcbd3ba6929484
debabrata-97 <debabrataszr@gmail.com>
2023-02-23 08:55:39 +0530

    Welcome to Employee Wage Computation

---
+++ src/Day3_Assignment_1/EmployeeWage.java
@@ -0,0 +1,17 @@
+package Day3_Assignment_1;
+
+public class EmployeeWage {
+    public static void main(String args[]) {
+        final int FULL_TIME = 1;
+        int empType = (int) (Math.random() * 100) % 2;
+        if (empType == FULL_TIME) {
+            System.out.println("Employee is Present");
+
+        } else {
+            System.out.println("Employee is Absent");
+
+        }
+    }
+}
+
+
