ce8621890a58581764f6b3a9d125e1bdcee5c8aa
Debabrata Priyadarshi Sahoo <124128301+debabrataszr@users.noreply.github.com>
2023-02-23 13:31:10 +0530

    Update EmployeeWage.java

--- src/Day3_Assignment_1/EmployeeWage.java
+++ src/Day3_Assignment_1/EmployeeWage.java
@@ -3,15 +3,7 @@ package Day3_Assignment_1;
 public class EmployeeWage {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Computation Problem");
-        final int FULL_TIME = 1;
-        int empType = (int) (Math.random() * 100) % 2;
-        if (empType == FULL_TIME) {
-            System.out.println("Employee is Present");
-
-        } else {
-            System.out.println("Employee is Absent");
-
-        }
+       
     }
 }
 
